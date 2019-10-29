8abbc4b6ebab8bda648013d0f3f75e72e59047eb
Tristan Wiesepape <wiesepapetristan@gmail.com>
2019-10-28 20:14:49 -0500

    added problem checking

---
+++ .workspace/Example_Compile.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Example_Compile
+{
+    public static void main(String[] args) throws Exception
+    {
+        new Example_Compile().run();
+    }
+    public void run() throws Exception
+    {
+        if a < b
+        Scanner file = new Scanner(new File("example.dat"));
+        while(file.hasNext()) System.out.println(file.nextLine());
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # CyWoodsContestProblems
+
+# Sample Problem Structure
+Example
+├── Example.docx
+├── Judge
+│   ├── example.dat
+│   └── example.out
+├── Sample
+│   ├── example.dat
+│   └── example.out
+└── Solutions
+    └── Example_Tristan.java
