a2844371199f11577d7972f60692600146065db7
Tristan Wiesepape <wiesepapetristan@gmail.com>
2019-11-21 14:22:36 -0600

    moved problem statements

---
+++ Snowballs/Solutions/Snowballs_Tristan.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class Snowballs_Tristan
+{
+    public static void main(String[]  args) throws Exception
+    {
+        new Snowballs_Tristan().run();
+    }
+    public void run() throws Exception
+    {
+        Scanner file = new Scanner(new File("Snowballs_Tristan.dat".toLowerCase()));
+    }
+}
