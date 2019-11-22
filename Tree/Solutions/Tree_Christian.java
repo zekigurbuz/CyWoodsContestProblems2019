3a189edae7e0eceb1fcfa1e0c681b5fa306d858e
Christian Bakhit <20789306+Starronix@users.noreply.github.com>
2019-11-22 14:39:04 -0600

    Add files via upload

---
+++ Tree/Solutions/Tree_Christian.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+import java.text.*;
+import java.math.*;
+import static java.lang.System.*;
+import static java.lang.Integer.*;
+import static java.lang.Double.*;
+import static java.lang.Math.*;
+
+			//change the class name
+public class Tree_Christian
+{
+	public void run() throws Exception
+	{
+		System.out.print("       *\r\n" + 
+				"      /|\\\r\n" + 
+				"     // \\\\\r\n" + 
+				"    //   \\\\\r\n" + 
+				"   //  o  \\\\\r\n" + 
+				"  //     o \\\\\r\n" + 
+				" //  o      \\\\\r\n" + 
+				"//___________\\\\\r\n" + 
+				"      |||\r\n" + 
+				"      |||\r\n" + 
+				"      |||\r\n" + 
+				"      |||\r\n" + 
+				"      ---");
+	}
+
+	public static void main(String[] args) throws Exception
+	{
+		new Tree_Christian().run();
+	}	
+	
+}
