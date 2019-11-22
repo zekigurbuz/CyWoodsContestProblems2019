72878d3d02c935910542eb104cb911fa2da3bbb4
Tristan Wiesepape <wiesepapetristan@gmail.com>
2019-11-22 14:40:05 -0600

    fixed script

---
+++ Tree/Solutions/Tree_Tristan.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class Tree_Tristan
+{
+    public static void main(String[]  args) throws Exception
+    {
+        new Tree_Tristan().run();
+    }
+    public void run() throws Exception
+    {
+        Scanner file = new Scanner(new File("Tree_Tristan.dat".toLowerCase()));
+    }
+}
