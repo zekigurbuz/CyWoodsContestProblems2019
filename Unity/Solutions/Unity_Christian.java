effdb0b3c5a56af7a09579713dbe6104071f418c
Christian Bakhit <20789306+Starronix@users.noreply.github.com>
2019-11-23 00:02:32 -0600

    Add files via upload

---
+++ Unity/Solutions/Unity_Christian.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+import java.text.*;
+import java.math.*;
+import static java.lang.System.*;
+import static java.lang.Integer.*;
+import static java.lang.Double.*;
+import static java.lang.Math.*;
+
+			//change the class name
+public class Unity_Christian
+{
+	public void run() throws Exception
+	{
+		Scanner file = new Scanner(new File("unity.dat"));
+		
+		int times = file.nextInt(); 
+		file.nextLine();
+		boolean fool = false;
+		for(int asdf = 0; asdf<times; asdf++)
+		{
+			if(file.nextLine().indexOf("Maxwell: Regarding the Unity update...") > -1)
+				fool=true;
+		}
+		if(fool)
+			System.out.println("Song Failed. Everyone gets an F in their stocking.");
+		else
+			System.out.print( "Song passed! Everyone gets an A for Christmas." );
+		}
+	public static void main(String[] args) throws Exception
+	{
+		new Unity_Christian().run();
+	}	
+	
+}
