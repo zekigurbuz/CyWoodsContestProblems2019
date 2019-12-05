e10edd222525d2796b48a8cd716f32ec76e63a01
Christian Bakhit <20789306+Starronix@users.noreply.github.com>
2019-12-04 21:59:08 -0600

    added fixed file

--- Like/Solutions/Like_Christian.java
+++ Like/Solutions/Like_Christian.java
@@ -12,17 +12,18 @@ public class Like_Christian
 	public void run() throws Exception
 	{
 		Scanner file = new Scanner(new File("like.dat"));
-		
+		int counter=0;
 		int times = file.nextInt(); 
 		file.nextLine();
 		for(int asdf = 0; asdf<times; asdf++)
 		{
 			String state = file.nextLine();
-			state = state.replaceAll("(Like |, like,|, like|like, |^like | $like|Um|, um,|, um|um, |^um | $um|Uh|, uh,|, uh|uh, |^uh | $uh)|", "");
-			state = state.replaceAll("like |um | uh", "");
+			state = state.replaceAll("(Like |, like,|, like|like, |^like | $like|Um |, um,|, um|um, |^um | $um|Uh |, uh,|, uh|uh, |^uh | $uh)|", "");
+			for(int x =0; x < 3;x++)
+			state = state.replaceAll(" like | uh | um ", " ");
 			System.out.println(state);
-			
-		}}
+			}
+	}
 	public static void main(String[] args) throws Exception
 	{
 		new Like_Christian().run();
