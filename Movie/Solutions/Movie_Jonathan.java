bea826e75866af103a2f0ec05d9e80fb07f089cd
Jonathan Bakhit <57812972+syncre02@users.noreply.github.com>
2019-12-06 00:17:12 -0600

    Add files via upload

---
+++ Movie/Solutions/Movie_Jonathan.java
@@ -0,0 +1,36 @@
+
+import java.util.*;
+import java.io.*;
+import java.text.*;
+import java.math.*;
+import static java.lang.System.*;
+import static java.lang.Integer.*;
+import static java.lang.Double.*;
+import static java.lang.Math.*;
+
+			//change the class name
+public class Movie_Jonathan
+{
+	public void run() throws Exception
+	{
+		System.out.print("       *\r\n" + 
+				"      /|\\\r\n" + 
+				"     // \\\\\r\n" + 
+				"    //   \\\\\r\n" + 
+				"   //  o  \\\\\r\n" + 
+				"  //     o \\\\\r\n" + 
+				" //  o      \\\\\r\n" + 
+				"//___________\\\\\r\n" + 
+				"      |||\r\n" + 
+				"      |||\r\n" + 
+				"      |||\r\n" + 
+				"      |||\r\n" + 
+				"      ---");
+	}
+
+	public static void main(String[] args) throws Exception
+	{
+		new Movie_Jonathan().run();
+	}	
+	
+}
