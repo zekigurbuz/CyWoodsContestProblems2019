5be1f25af3c51a1277e0ecdd275d8b72479789ed
alex <alexhuang6316.ah@gmail.com>
2019-12-06 19:06:31 -0600

    18
    * What:
    
    * Why:
    
    * How:

---
+++ Basket/Solutions/Basket_Alex.java
@@ -0,0 +1,5 @@
+public class Basket_Alex {
+	public static void main (String[] args) {
+		System.out.println("EEEEEEEEEEEEEEEEE\nE     -----     E\nE       X       E\nE     -----     E\nEEEEXEEEXEEEXEEEE");
+	}
+}
