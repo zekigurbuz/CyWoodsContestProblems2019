44256de65aecca69dc148f9eb28fe9eb478abd96
Tristan Wiesepape <wiesepapetristan@gmail.com>
2019-12-10 10:23:42 -0600

    solved basket

---
+++ Basket/Solutions/Basket_Tristan.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+public class Basket_Tristan
+{
+    public static void main(String[]  args) throws Exception
+    {
+        new Basket_Tristan().run();
+    }
+    public void run() throws Exception
+    {
+        System.out.println(
+"EEEEEEEEEEEEEEEEE\n"+
+"E     -----     E\n"+
+"E       X       E\n"+
+"E     -----     E\n"+
+"EEEEXEEEXEEEXEEEE\n");
+    }
+}
